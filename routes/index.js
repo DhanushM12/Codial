ff7c92c31f65e294307b6673ab834f3b27d175fb
DhanushM12 <dhanushmukhi@gmail.com>
2020-03-24 20:10:57 +0530

    added ejs

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,7 @@ console.log("router loaded");
 router.get("/", homeController.home);
 router.use("/users", require("./users"));
 
+//for any other routes, access from here
+//router.use('/routerName', require('./routerFile'));
+
 module.exports = router;
