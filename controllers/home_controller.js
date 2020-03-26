876d7fae82247df32caed9c2f3da5dfc8fd095f3
DhanushM12 <dhanushmukhi@gmail.com>
2020-03-27 03:49:34 +0530

    setup static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res) {
-  return res.end("<h1>Express is up for Codial</h1>");
+  //return res.end("<h1>Express is up for Codial</h1>");
+  return res.render("home", {
+    title: "Home"
+  });
 };
 
 //module.exports.actionName = frunction(req,res){}
