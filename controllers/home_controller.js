f69a6c07007ec099763bfe97426987590105fda8
DhanushM12 <dhanushmukhi@gmail.com>
2020-03-31 17:11:36 +0530

    Posts are visible on the homepage and the post creator also name is mentioned

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post = require("../models/post");
+
 module.exports.home = function(req, res) {
   //return res.end("<h1>Express is up for Codial</h1>");
-  console.log(req.cookies);
-  res.cookie("user_id", 25);
-  return res.render("home", {
-    title: "Home"
-  });
+  //console.log(req.cookies);
+  //res.cookie("user_id", 25);
+  // Post.find({}, function(err, posts) {
+  //   return res.render("home", {
+  //    title: "Codial | Home",
+  //    posts: posts
+  //  });
+  //});
+
+  //populate the user for each post
+  Post.find({})
+    .populate("user")
+    .exec(function(err, posts) {
+      return res.render("home", {
+        title: "Codial | Home",
+        posts: posts
+      });
+    });
 };
 
 //module.exports.actionName = frunction(req,res){}
