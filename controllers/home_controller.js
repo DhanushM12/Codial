c7e1f49f97cc8ec598e013ca92ca6aba65688062
DhanushM12 <dhanushmukhi@gmail.com>
2020-04-03 03:14:33 +0530

    Comments added under post and successfully added to db for comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,6 +14,12 @@ module.exports.home = function(req, res) {
   //populate the user for each post
   Post.find({})
     .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user"
+      }
+    })
     .exec(function(err, posts) {
       return res.render("home", {
         title: "Codial | Home",
