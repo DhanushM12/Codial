4d58b7520f70d6cd4f7f88f0c4c6b3fc9fc507ad
DhanushM12 <dhanushmukhi@gmail.com>
2020-04-03 15:08:55 +0530

    Users profile links are available and User canalso update its own details

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const User = require("../models/user");
 
 module.exports.home = function(req, res) {
   //return res.end("<h1>Express is up for Codial</h1>");
@@ -21,9 +22,12 @@ module.exports.home = function(req, res) {
       }
     })
     .exec(function(err, posts) {
-      return res.render("home", {
-        title: "Codial | Home",
-        posts: posts
+      User.find({}, function(err, users) {
+        return res.render("home", {
+          title: "Codial | Home",
+          posts: posts,
+          all_users: users
+        });
       });
     });
 };
