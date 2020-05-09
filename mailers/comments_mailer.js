dce9a9802f772ae8bfc7e23ba523b45f31112b9d
DhanushM12 <dhanushmukhi@gmail.com>
2020-05-10 02:54:08 +0530

    Template added to the mailer

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,8 +3,11 @@ const nodemailer = require("../config/nodemailer");
 //this is another way of exporting a method
 //this newComment method should be called when a new comment is made so we update in comments controller
 exports.newComment = (comment) => {
-  console.log("inside new comment mailer");
-
+  //console.log("inside new comment mailer");
+  let htmlString = nodemailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
   //sendmail is predefined function
   nodemailer.transporter.sendMail(
     {
@@ -12,7 +15,7 @@ exports.newComment = (comment) => {
       to: comment.user.email, // list of receivers
       subject: "New Comment Published", // Subject line
       // text: "Hello world?", // plain text body
-      html: "<h1>Yep, your comment is now published!!!</h1>", // html body
+      html: htmlString, //"<h1>Yep, your comment is now published!!!</h1>", // html body
     },
     (err, info) => {
       if (err) {
