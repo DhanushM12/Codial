3988d69d81989585531101b71b79090c16e8967a
DhanushM12 <dhanushmukhi@gmail.com>
2020-05-10 20:56:40 +0530

    Added kue and worker for jobs to be performed

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const commentMailers = require("../mailers/comments_mailer");
+const queue = require("../config/kue");
+const commentEmailWorker = require("../workers/comment_email_worker");
 
 module.exports.create = async function (req, res) {
   try {
@@ -16,7 +18,14 @@ module.exports.create = async function (req, res) {
       post.comments.push(comment);
       post.save();
       comment = await comment.populate("user", "name email").execPopulate();
-      commentMailers.newComment(comment);
+      //commentMailers.newComment(comment);
+      let job = queue.create("emails", comment).save(function (err) {
+        if (err) {
+          console.log("error in sending to the queue", err);
+          return;
+        }
+        console.log("job enqueued", job.id);
+      });
       if (req.xhr) {
         return res.status(200).json({
           data: {
