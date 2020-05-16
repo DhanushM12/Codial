2dec1987fce5f7db1841ddc63d9e52ece7db4968
DhanushM12 <dhanushmukhi@gmail.com>
2020-05-16 18:34:47 +0530

    chatting engine working

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,1 +1,23 @@
-module.exports.chatSockets = function (socketServer) {};
+module.exports.chatSockets = function (socketServer) {
+  let io = require("socket.io")(socketServer);
+
+  io.sockets.on("connection", function (socket) {
+    console.log("new connection received", socket.id);
+
+    socket.on("disconnect", function () {
+      console.log("socket disconnected!");
+    });
+
+    socket.on("join_room", function (data) {
+      console.log("joining request rec.", data);
+      //user joining the chatroom
+      socket.join(data.chatroom);
+      //user has joined the chatroom notified to all
+      io.in(data.chatroom).emit("user_joined", data);
+    });
+    // detect send_message and broadcast to everyone in the room
+    socket.on("send_message", function (data) {
+      io.in(data.chatroom).emit("receive_message", data);
+    });
+  });
+};
