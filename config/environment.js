33d77b2778792719d3915f591766c9e01f0cfce2
DhanushM12 <dhanushmukhi@gmail.com>
2020-06-04 17:41:54 +0530

    updated environment config

--- config/environment.js
+++ config/environment.js
@@ -21,16 +21,15 @@ const development = {
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "dhanushmukhi", // generated username
-      pass: "Dhanush@1297#", // generated password
+      user: "####", // your gmail username
+      pass: "####", // your gmail password
     },
     tls: {
       rejectUnauthorized: false,
     },
   },
-  google_client_id:
-    "273710427537-s6hqhoqr8sahh7igmjifabfgbiut3g5o.apps.googleusercontent.com",
-  google_client_secret: "B-xfUk8LleK0VWho7-RGzbUL",
+  google_client_id: "####",
+  google_client_secret: "####",
   google_call_back_url: "http://localhost:8000/users/auth/google/callback",
   jwt_secret: "codial",
   morgan: {
